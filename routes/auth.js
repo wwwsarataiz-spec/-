a768281fe878ba7ae73f626b91583c10e41d783c
ساره ندور <wwwsarataiz@gmail.com>
2026-06-24 21:51:44 +0300

    Delete routes directory

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// مسار تجريبي لتسجيل الحسابات
-router.post('/register', (req, res) => {
-    res.json({ success: true, message: "تم تسجيل الحساب بنجاح في نظام Nexora" });
-});
-
-module.exports = router;
