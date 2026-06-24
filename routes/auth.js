e80df9ad966af120139a0e11af32201897ddb0b6
ساره ندور <wwwsarataiz@gmail.com>
2026-06-24 21:36:22 +0300

    Delete routes directory

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/login-test', (req, res) => {
-    res.json({ message: "مرحباً بك! نظام تسجيل الدخول يعمل بنجاح." });
-});
-
-module.exports = router;
