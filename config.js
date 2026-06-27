0fe5f8a71fe3e834fbe3f6faad2f7f96ad61d0e8
ساره ندور <wwwsarataiz@gmail.com>
2026-06-27 17:10:34 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    mining: {
+        clickReward: 0.001,
+        clickBonus: 0.005,
+        dailyClicks: 100,
+        freeRoundEvery: 50
+    },
+
+    casino: {
+        houseEdge: 3
+    },
+
+    market: {
+        commission: 0.05
+    },
+
+    wallet: {
+        minWithdraw: 5,
+        minDeposit: 1
+    }
+
+};
